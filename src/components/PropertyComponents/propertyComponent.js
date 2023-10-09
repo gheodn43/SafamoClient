d55a917c21b7c14b3a8b0edbfb35803f2d2c9a0c
gheodn43 <97235672+gheodn43@users.noreply.github.com>
2023-10-09 15:20:19 -0500

    add feature send landlord request

--- src/components/PropertyComponents/propertyComponent.js
+++ src/components/PropertyComponents/propertyComponent.js
@@ -1,21 +1,25 @@
 import React, { useState } from 'react';
 import PropertyTable from '../DataDisplayComponents/Tables/propertyTable';
 import PropertyCreation from './CreatePropertyComponent/PropertyCreation';
+import CreateLandlordRequest from '../RequestComponents/createLandlordRequest';
 
 const PropertyComponent = () => {
   const [isCreationVisible, setIsCreationVisible] = useState(false);
-
+  const [isRequestFormVisible, setIsRequestFormVisible] = useState(false);
   const handleCreatePropertyClick = () => {
     setIsCreationVisible(true);
+  };
+  const handleCreateLandlordRequest = () => {
+    setIsRequestFormVisible(true);
   };
 
   const handleHideCreation = () => {
     setIsCreationVisible(false);
+    setIsRequestFormVisible(false);
   };
 
   const handleOverlayClick = () => {
-    // Đóng phần PropertyCreation khi người dùng nhấp ra ngoài phạm vi
-    if (isCreationVisible) {
+    if (isCreationVisible || isRequestFormVisible) {
       handleHideCreation();
     }
   };
@@ -24,8 +28,17 @@ const PropertyComponent = () => {
     <div className="property-component">
       <div className='container'>
         <div className="property-header row">
-          <h2 className='col-sm-9'>Bất động sản</h2>
-          <div className='col-sm'>
+          <div className='col-sm-7'>
+            <h2>Bất động sản</h2>
+            <div className="alert alert-warning" role="alert">
+              Tài khoản chưa được mở khóa chức năng cho thuê.
+              <button type="button" className="btn btn-outline-secondary" 
+              onClick={handleCreateLandlordRequest} style={{ margin: '0 0 0 5px' }}>Gửi yêu cầu nâng cấp ngay !</button>
+            </div>
+          </div>
+
+          <div className='col-sm-3'></div>
+          <div className='col-sm-2'>
             <button className="btn btn-primary" onClick={handleCreatePropertyClick}>Đăng ký mới Bất động sản</button>
           </div>
         </div>
@@ -37,6 +50,18 @@ const PropertyComponent = () => {
               <div className="property-creation">
                 <button className="btn btn-danger" onClick={handleHideCreation}>Đóng</button>
                 <PropertyCreation />
+              </div>
+            </>
+          )}
+        </div>
+
+        <div className="landlordReq-content">
+          {isRequestFormVisible && (
+            <>
+              <div className="overlay" onClick={handleOverlayClick}></div>
+              <div className="landlordReq-creation">
+                <button className="btn btn-danger" onClick={handleHideCreation}>Đóng</button>
+                <CreateLandlordRequest />
               </div>
             </>
           )}
